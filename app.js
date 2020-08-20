3b71d52d8096c193fb7ee8f6cbe48aad4f4751bb
facebookonlinegames001 <69955713+facebookonlinegames001@users.noreply.github.com>
2020-08-20 12:57:33 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,15 +3,15 @@
 
 ///Edit Under This only
 
-var firebaseConfig = {
-  apiKey: "api-key",
-  authDomain: "project-id.firebaseapp.com",
-  databaseURL: "https://project-id.firebaseio.com",
-  projectId: "project-id",
-  storageBucket: "project-id.appspot.com",
-  messagingSenderId: "sender-id",
-  appId: "app-id",
-  measurementId: "G-measurement-id",
+const firebaseConfig = {
+  apiKey: "AIzaSyDD5MPXKS35G5xiEDGq1DMbki3_4jvfelk",
+  authDomain: "facebookonlinegames-263df.firebaseapp.com",
+  databaseURL: "https://facebookonlinegames-263df.firebaseio.com",
+  projectId: "facebookonlinegames-263df",
+  storageBucket: "facebookonlinegames-263df.appspot.com",
+  messagingSenderId: "204080042792",
+  appId: "1:204080042792:web:e75996feb60387cd75361d",
+  measurementId: "G-6EL4DEFP5T"
 };
 
      
